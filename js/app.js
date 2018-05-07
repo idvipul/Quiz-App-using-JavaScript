5557a0918e60e79cfa288ea2ce843267323247e1
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-05-06 19:16:34 -0700

    questions added in app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// add questions to array
+var questions = [
+    new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C", "C++"], "C"),
+    new Question("Which language is used for styling web pages?", ["CSS", "HTML", "jQuery", "XML"], "CSS"),
+    new Question("There are __ main components of object oriented programming", ["1", "2", "4", "6"], "4"),
+    new Question("The Bootstrap grid system is based on how many columns?", ["3", "6", "12", "18"], "12"),
+    new Question("MVC is a ________.", ["Website", "Library", "Language", "Framework"], "Framework")
+]
+
