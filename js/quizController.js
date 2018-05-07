f4cd32b2939df99bd03e99df4aba3b3ea367c0da
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-05-06 18:29:57 -0700

    js files added

---
+++ js/quizController.js
@@ -0,0 +1,24 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+// get index of current question
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+// check is quiz has ended or not
+Quiz.prototype.isEnded = function() {
+    return this.questions.length ===this.questionIndex;
+}
+
+// calculate score
+Quiz.prototype.guess = function (answer) {
+    this.questionIndex++;
+
+    if(this.questionIndex().correctAnswer(answer)) {
+        this.score++;
+    }
+}
